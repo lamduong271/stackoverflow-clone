7b078585b305c4f6ed07bed02642b2d7019b067b
lamduong96 <lam.duong@smartly.io>
2022-07-18 11:54:21 +0300

    rm tsc

--- src/config/mongoConfig.ts
+++ src/config/mongoConfig.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI!);
-  console.log("MongoDb Connected");
+  console.log("MongoDb database Connected");
 };
