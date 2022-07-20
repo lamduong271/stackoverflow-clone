e6934acfadeac62fcb6aacee1f14ddad6a6b4e46
lamduong96 <lam.duong@smartly.io>
2022-07-21 00:08:58 +0300

    get user

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -60,8 +60,9 @@ router.get("/:id", verifyToken, async (req: Request, res: Response) => {
     const existUser = await User.findOne({ email: req.params.id });
 
     if (existUser) {
-      res.status(401).send({ name: existUser.name, email: existUser.email });
+      res.status(200).send({ name: existUser.name, email: existUser.email });
     }
+    res.status(401).send({ status: false, message: "not found user" });
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server error Occured");
